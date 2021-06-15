403602e3d0acec939d3118feb254948390bbddfa
Martin Tournoij <martin@arp242.net>
2021-06-16 04:56:34 +0800

    Split out command to ./cmd/tomltest; various small improvements
    Split out the test runner and actual command to tomltest and
    ./cmd/tomltest packages. The practical upshot of this is that you can
    call the rest runner directly from your tests; removing the need to
    install an external binary, and you should get some code coverage for it
    as well. While only useful for Go libraries, it's a nice thing for the
    toml tests and won't hurt anyone else.
    
    Rewrite the CLI to use my little zgo.at/zli package; the flag package in
    Go is finicky and even the standard "go" commands contain all sorts of
    hacks to work around the problems. For example you can't have flags
    after positional arguments, and adding "-v" a chore rather than
    <Up>v<Enter>.
    
    This fixes #9 by passing taking all of os.Args[1:] as the parser-cmd:
    
    	$ toml-encoder my-decoder my-decoder-arg
    	$ toml-encoder -- my-decoder -x  # -- to stop flag parsing.
    
    List the tests to run with -run=.. – there wasn't really any good way to
    fix it otherwise; quoting could work (toml-test 'my-decoder -x'), but
    shell quoting/word splitting is hairy and I'd rather not inflict that
    upon people (see: the original space-separated -tags for the go command,
    could be really hard to script with that).
    
    Also accept glob patterns for -run and -skip.
    
    Further improve the output and expand the docs a bit, and a few other
    minor things.

--- json.go
+++ json.go
@@ -1,4 +1,4 @@
-package main
+package tomltest
 
 import (
 	"strconv"
@@ -6,26 +6,26 @@ import (
 	"time"
 )
 
-// cmpJSON consumes the recursive structure of both `want` and `have`
+// cmpJSON consumes the recursive structure of both want and have
 // simultaneously. If anything is unequal, the result has failed and comparison
 // stops.
 //
-// N.B. `reflect.DeepEqual` could work here, but it won't tell us how the two
+// reflect.DeepEqual could work here, but it won't tell us how the two
 // structures are different.
-func (r result) cmpJSON(want, have interface{}) result {
+func (r Test) cmpJSON(want, have interface{}) Test {
 	switch w := want.(type) {
 	case map[string]interface{}:
 		return r.cmpJSONMaps(w, have)
 	case []interface{}:
 		return r.cmpJSONArrays(w, have)
 	default:
-		return r.failedf(
+		return r.fail(
 			"Key '%s' in expected output should be a map or a list of maps, but it's a %T",
-			r.key, want)
+			r.Key, want)
 	}
 }
 
-func (r result) cmpJSONMaps(want map[string]interface{}, have interface{}) result {
+func (r Test) cmpJSONMaps(want map[string]interface{}, have interface{}) Test {
 	haveMap, ok := have.(map[string]interface{})
 	if !ok {
 		return r.mismatch("table", want, haveMap)
@@ -33,14 +33,14 @@ func (r result) cmpJSONMaps(want map[string]interface{}, have interface{}) resul
 
 	// Check to make sure both or neither are values.
 	if isValue(want) && !isValue(haveMap) {
-		return r.failedf(
+		return r.fail(
 			"Key '%s' is supposed to be a value, but the parser reports it as a table",
-			r.key)
+			r.Key)
 	}
 	if !isValue(want) && isValue(haveMap) {
-		return r.failedf(
+		return r.fail(
 			"Key '%s' is supposed to be a table, but the parser reports it as a value",
-			r.key)
+			r.Key)
 	}
 	if isValue(want) && isValue(haveMap) {
 		return r.cmpJSONValues(want, haveMap)
@@ -50,62 +50,62 @@ func (r result) cmpJSONMaps(want map[string]interface{}, have interface{}) resul
 	for k := range want {
 		if _, ok := haveMap[k]; !ok {
 			bunk := r.kjoin(k)
-			return bunk.failedf("Could not find key '%s' in parser output.",
-				bunk.key)
+			return bunk.fail("Could not find key '%s' in parser output.",
+				bunk.Key)
 		}
 	}
 	for k := range haveMap {
 		if _, ok := want[k]; !ok {
 			bunk := r.kjoin(k)
-			return bunk.failedf("Could not find key '%s' in expected output.",
-				bunk.key)
+			return bunk.fail("Could not find key '%s' in expected output.",
+				bunk.Key)
 		}
 	}
 
 	// Okay, now make sure that each value is equivalent.
 	for k := range want {
-		if sub := r.kjoin(k).cmpJSON(want[k], haveMap[k]); sub.failed() {
+		if sub := r.kjoin(k).cmpJSON(want[k], haveMap[k]); sub.Failed() {
 			return sub
 		}
 	}
 	return r
 }
 
-func (r result) cmpJSONArrays(want, have interface{}) result {
+func (r Test) cmpJSONArrays(want, have interface{}) Test {
 	wantSlice, ok := want.([]interface{})
 	if !ok {
-		return r.bugf("'value' should be a JSON array when 'type=array', but it is a %T", want)
+		return r.bug("'value' should be a JSON array when 'type=array', but it is a %T", want)
 	}
 
 	haveSlice, ok := have.([]interface{})
 	if !ok {
-		return r.failedf(
+		return r.fail(
 			"Malformed output from your encoder: 'value' is not a JSON array: %T", have)
 	}
 
 	if len(wantSlice) != len(haveSlice) {
-		return r.failedf("Array lengths differ for key '%s':\n"+
+		return r.fail("Array lengths differ for key '%s':\n"+
 			"  Expected:     %d\n"+
 			"  Your encoder: %d",
-			r.key, len(wantSlice), len(haveSlice))
+			r.Key, len(wantSlice), len(haveSlice))
 	}
 	for i := 0; i < len(wantSlice); i++ {
-		if sub := r.cmpJSON(wantSlice[i], haveSlice[i]); sub.failed() {
+		if sub := r.cmpJSON(wantSlice[i], haveSlice[i]); sub.Failed() {
 			return sub
 		}
 	}
 	return r
 }
 
-func (r result) cmpJSONValues(want, have map[string]interface{}) result {
+func (r Test) cmpJSONValues(want, have map[string]interface{}) Test {
 	wantType, ok := want["type"].(string)
 	if !ok {
-		return r.bugf("'type' should be a string, but it is a %T", want["type"])
+		return r.bug("'type' should be a string, but it is a %T", want["type"])
 	}
 
 	haveType, ok := have["type"].(string)
 	if !ok {
-		return r.failedf("Malformed output from your encoder: 'type' is not a string: %T", have["type"])
+		return r.fail("Malformed output from your encoder: 'type' is not a string: %T", have["type"])
 	}
 
 	if wantType != haveType {
@@ -120,12 +120,12 @@ func (r result) cmpJSONValues(want, have map[string]interface{}) result {
 	// Atomic values are always strings
 	wantVal, ok := want["value"].(string)
 	if !ok {
-		return r.bugf("'value' should be a string, but it is a %T", want["value"])
+		return r.bug("'value' should be a string, but it is a %T", want["value"])
 	}
 
 	haveVal, ok := have["value"].(string)
 	if !ok {
-		return r.failedf("Malformed output from your encoder: %T is not a string", have["value"])
+		return r.fail("Malformed output from your encoder: %T is not a string", have["value"])
 	}
 
 	// Excepting floats and datetimes, other values can be compared as strings.
@@ -139,43 +139,43 @@ func (r result) cmpJSONValues(want, have map[string]interface{}) result {
 	}
 }
 
-func (r result) cmpAsStrings(want, have string) result {
+func (r Test) cmpAsStrings(want, have string) Test {
 	if want != have {
-		return r.failedf("Values for key '%s' don't match:\n"+
+		return r.fail("Values for key '%s' don't match:\n"+
 			"  Expected:     %s\n"+
 			"  Your encoder: %s",
-			r.key, want, have)
+			r.Key, want, have)
 	}
 	return r
 }
 
-func (r result) cmpFloats(want, have string) result {
+func (r Test) cmpFloats(want, have string) Test {
 	// Special case for NaN, since NaN != NaN.
 	if strings.HasSuffix(want, "nan") || strings.HasSuffix(have, "nan") {
 		if want != have {
-			return r.failedf("Values for key '%s' don't match:\n"+
+			return r.fail("Values for key '%s' don't match:\n"+
 				"  Expected:     %v\n"+
 				"  Your encoder: %v",
-				r.key, want, have)
+				r.Key, want, have)
 		}
 		return r
 	}
 
 	wantF, err := strconv.ParseFloat(want, 64)
 	if err != nil {
-		return r.bugf("Could not read '%s' as a float value for key '%s'", want, r.key)
+		return r.bug("Could not read '%s' as a float value for key '%s'", want, r.Key)
 	}
 
 	haveF, err := strconv.ParseFloat(have, 64)
 	if err != nil {
-		return r.failedf("Malformed output from your encoder: key '%s' is not a float: '%s'", r.key, have)
+		return r.fail("Malformed output from your encoder: key '%s' is not a float: '%s'", r.Key, have)
 	}
 
 	if wantF != haveF {
-		return r.failedf("Values for key '%s' don't match:\n"+
+		return r.fail("Values for key '%s' don't match:\n"+
 			"  Expected:     %v\n"+
 			"  Your encoder: %v",
-			r.key, wantF, haveF)
+			r.Key, wantF, haveF)
 	}
 	return r
 }
@@ -192,7 +192,7 @@ var layouts = map[string]string{
 	"time-local":     "15:04:05",
 }
 
-func (r result) cmpAsDatetimes(kind, want, have string) result {
+func (r Test) cmpAsDatetimes(kind, want, have string) Test {
 	layout, ok := layouts[kind]
 	if !ok {
 		panic("should never happen")
@@ -200,28 +200,37 @@ func (r result) cmpAsDatetimes(kind, want, have string) result {
 
 	wantT, err := time.Parse(layout, datetimeRepl.Replace(want))
 	if err != nil {
-		return r.bugf("Could not read '%s' as a datetime value for key '%s'", want, r.key)
+		return r.bug("Could not read '%s' as a datetime value for key '%s'", want, r.Key)
 	}
 
 	haveT, err := time.Parse(layout, datetimeRepl.Replace(want))
 	if err != nil {
-		return r.failedf("Malformed output from your encoder: key '%s' is not a datetime: '%s'", r.key, have)
+		return r.fail("Malformed output from your encoder: key '%s' is not a datetime: '%s'", r.Key, have)
 	}
 	if !wantT.Equal(haveT) {
-		return r.failedf("Values for key '%s' don't match:\n"+
+		return r.fail("Values for key '%s' don't match:\n"+
 			"  Expected:     %v\n"+
 			"  Your encoder: %v",
-			r.key, wantT, haveT)
+			r.Key, wantT, haveT)
 	}
 	return r
 }
 
-func (r result) cmpAsDatetimesLocal(want, have string) result {
+func (r Test) cmpAsDatetimesLocal(want, have string) Test {
 	if datetimeRepl.Replace(want) != datetimeRepl.Replace(have) {
-		return r.failedf("Values for key '%s' don't match:\n"+
+		return r.fail("Values for key '%s' don't match:\n"+
 			"  Expected:     %v\n"+
 			"  Your encoder: %v",
-			r.key, want, have)
+			r.Key, want, have)
+	}
+	return r
+}
+
+func (r Test) kjoin(key string) Test {
+	if len(r.Key) == 0 {
+		r.Key = key
+	} else {
+		r.Key += "." + key
 	}
 	return r
 }
@@ -237,4 +246,18 @@ func isValue(m map[string]interface{}) bool {
 		return false
 	}
 	return true
+}
+
+func (r Test) mismatch(wantType string, want, have interface{}) Test {
+	return r.fail("Key '%s' is not an %s but %[4]T:\n"+
+		"  Expected:     %#[3]v\n"+
+		"  Your encoder: %#[4]v",
+		r.Key, wantType, want, have)
+}
+
+func (r Test) valMismatch(wantType, haveType string, want, have interface{}) Test {
+	return r.fail("Key '%s' is not an %s but %s:\n"+
+		"  Expected:     %#[3]v\n"+
+		"  Your encoder: %#[4]v",
+		r.Key, wantType, want, have)
 }
