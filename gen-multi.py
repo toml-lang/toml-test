d77cf553ff03624bc0e89441fd706e2cacbe6155
Martin Tournoij <martin@arp242.net>
2022-01-05 15:17:07 +0100

    Encode control characters as \x.. in .multi files
    This makes control character tests a lot easier to work with.

--- gen-multi.py
+++ gen-multi.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-import glob
-import os.path
+import glob, os.path, re
 
 for f in glob.glob('tests/invalid/*/*.multi'):
     base = os.path.dirname(f[:-6])
-    for l in open(f, 'rb').readlines():
-        name = l.split(b'=')[0].strip().decode()
+    for line in open(f, 'rb').readlines():
+        name = line.split(b'=')[0].strip().decode()
         if name == '' or name[0] == '#':
             continue
+
+        line = re.sub(r'(?<=[^\\])\\x([0-9a-fA-F]{2})', lambda m: chr(int(m[1], 16)), line.decode())
         path = base + "/" + name + '.toml'
         with open(path, 'wb+') as fp:
-            fp.write(l)
+            fp.write(line.encode())
